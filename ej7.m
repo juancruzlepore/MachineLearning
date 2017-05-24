d1a5ba66cddd02b4babfb7d9c8526d488c1354e1
Juan Dantur <jpdantur@gmail.com>
2017-05-24 19:00:35 -0300

    Add files via upload

---
+++ ej7.m
@@ -0,0 +1,19 @@
+%{
+
+	7.a y 7.b/c)
+	Los graficos fueron realizados via interfaz grafica.
+
+	7.d)
+	R^2 = 0.9988 para el polinomio cúbico
+	R^2 = 0.9987 para el polinomio cuadrático
+
+	Esto nos dice que el polinomio cúbico se ajusta mejor a los datos dados.
+
+	7.e)
+	El modelo cúbico predijo 277 millones
+
+	El modelo cuadrático predijo 275 millones
+
+	Por lo tanto, el modelo cúbico estuvo más cerca de la respuesta correcta.
+
+}
