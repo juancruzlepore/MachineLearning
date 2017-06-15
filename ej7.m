b8f66eef7e00a067ce3dade636576d18dc4bbe73
Juan Dantur <jpdantur@gmail.com>
2017-06-15 13:32:51 -0300

    Create ej7.m

--- ej7.m
+++ ej7.m
@@ -1,7 +1,16 @@
-%{
+load census;
+%7.a) No existe relacion lineal entre las variables
+scatter(cdate,pop)
 
-	7.a y 7.b/c)
-	Los graficos fueron realizados via interfaz grafica.
+%7.b) Ajustar polinomio
+polyfit(cdate,pop,2)
+%Coeficientes: [6.5411e-03  -2.3510e+01   2.1130e+04]
+
+%7.c) Ajustar polinomio
+polyfit(cdate,pop,3)
+%Coeficientes: [3.8555e-06  -1.5319e-02   1.7781e+01  -4.8519e+03]
+%TODO: Estudiar residuos
+%{
 
 	7.d)
 	R^2 = 0.9988 para el polinomio cúbico
